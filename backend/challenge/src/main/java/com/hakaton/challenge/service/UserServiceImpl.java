b1d16e6681d835d8c521908382623f3cf255e4a2
Marija Klještan <marijakljestan@uns.ac.rs>
2022-11-19 18:13:05 +0100

    fix: Model mapper

--- backend/challenge/src/main/java/com/hakaton/challenge/service/UserServiceImpl.java
+++ backend/challenge/src/main/java/com/hakaton/challenge/service/UserServiceImpl.java
@@ -3,24 +3,22 @@ import com.hakaton.challenge.api.User;
 import com.hakaton.challenge.domain.UserEntity;
 import com.hakaton.challenge.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
 
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements  UserService{
+
     private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
     public User saveUser(User u)
     {
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(u.getId());
-        userEntity.setEmail(u.getEmail());
-        userEntity.setFirstName(u.getFirstName());
-        userEntity.setLastName(u.getLastName());
-        userEntity  = userRepository.save(userEntity);
-
-        //u.setId(userEntity.getId());
-
-        return u;
+          UserEntity userEntity = new UserEntity();
+          userEntity = modelMapper.map(u, UserEntity.class);
+          userEntity  = userRepository.save(userEntity);
+          return u;
     }
 
 }
